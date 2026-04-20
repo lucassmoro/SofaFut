a4fe4b13743649320a706c4dbce4a1865e6422f1
lucassmoro <lucas.sm.souza@gmail.com>
2026-04-20 01:02:16 -0300

    atributos pontuacao e saldo ao cliente

--- src/models/client.py
+++ src/models/client.py
@@ -5,12 +5,14 @@ import SofaFut.src.external.sofascore_api as sofascore_api
 from repositories.users_database import UserDataBase
 
 class Client:
-    def __init__(self, nome, cpf, email, senha):
+    def __init__(self, nome, cpf, email, senha, pontuacao, saldo):
 
         self.nome = nome
         self.cpf = cpf
         self.email = email
         self.__senha = self._generate_hash(senha)
+        self.pontuacao = 0
+        self.saldo = 0
 
     def _generate_hash(self, senha):
         return hashlib.sha256(senha.encode()).hexdigest()
