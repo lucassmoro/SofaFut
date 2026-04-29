b33d041aaa172755904eebc4eb8daf5fc0f2095f
lucassmoro <lucas.sm.souza@gmail.com>
2026-04-29 20:52:44 -0300

    construtor do PlayerLineup (classe que do jogador escalado)

--- src/models/player_lineup.py
+++ src/models/player_lineup.py
@@ -1,4 +1,11 @@
 class PlayerLineup:
 
     def __init__(self, jogador, capitao, atuou, titular, pontuacao):
-        
+    
+        self.jogador = jogador
+        self.capitao = capitao
+        self.atuou = atuou
+        self.titular = titular
+        self.pontuacao = pontuacao
+
+    
