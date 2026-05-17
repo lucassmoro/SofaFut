a371c0ec4f2f4cfd5cb3f1a5c89b689ee788d4cf
lucassmoro <lucas.sm.souza@gmail.com>
2026-05-17 17:59:21 -0300

    calculo de pontuacao da escalacao agora filtra os jogadores escalados

--- src/services/fantasy_score_service.py
+++ src/services/fantasy_score_service.py
@@ -9,12 +9,17 @@ from src.repositories.rounds_repository import RoundRepository
 class FantasyScoreService:
 
     def calcular_pontuacao_lineup(self, escalacao : Lineup, rodadas_repo : RoundRepository):
-        
+
         round = rodadas_repo.buscar_por_numero(escalacao.rodada)
 
         for partida in round.partidas:
-            for jogador in partida.jogadores_partida:
-                self.calculo_pontuacao_logica(jogador)
+            for jogador in partida.jogadores_partida: # acessa os players_match
+                for jogador_fantasy in escalacao.jogadores: # acesssa os player_fantasy
+                    
+                    if jogador_fantasy.jogador == jogador.jogador: # verifica se ambos player_ referenciam o mesmo jogador
+                        escalacao.pontuacao += self.calculo_pontuacao_logica(jogador)
+
+        return escalacao.pontuacao
 
     def calculo_pontuacao_logica(self, jogador : MatchPlayerStats):
         pass
